b741e712e4be09603cc30818ca5e199b19dc43e8
Lucas Gabriel <lucasgsantos1727@gmail.comm>
2023-01-16 10:54:37 -0300

    alteração de textos

--- src/components/last/Parteum/Parteum.jsx
+++ src/components/last/Parteum/Parteum.jsx
@@ -13,7 +13,7 @@ function Parteum() {
                     <div className={styles.sinopse}>
                         <div className={styles.foto} />
                         <div className={styles.conteudo}>
-                            Uma surtagem de uma mutação do fungo Cordyceps arrasa os Estados Unidos em setembro de 2013, transformando seus hospedeiros humanos em monstros canibalísticos chamados de Infectados. Joel foge do caos dos subúrbios de Austin, Texas, junto com seu irmão Tommy e sua filha Sarah. Esta acaba baleada por um soldado durante a fuga e morre nos braços do pai. A maior parte da civilização humana acaba destruída no decorrer dos vinte anos seguintes. Sobreviventes vivem em zonas de quarentena altamente policiadas, em assentamentos independentes e em grupos nômades.
+                            Um surto de uma mutação do fungo Cordyceps arrasa os Estados Unidos em setembro de 2013, transformando seus hospedeiros humanos em monstros canibalísticos chamados de Infectados. Joel foge do caos dos subúrbios de Austin, Texas, junto com seu irmão Tommy e sua filha Sarah. Esta acaba baleada por um soldado durante a fuga e morre nos braços do pai. A maior parte da civilização humana acaba destruída no decorrer dos vinte anos seguintes. Sobreviventes vivem em zonas de quarentena altamente policiadas, em assentamentos independentes e em grupos nômades.
                         </div>
                     </div>
 
